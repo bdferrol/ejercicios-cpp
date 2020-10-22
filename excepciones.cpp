9acba130673defe5e56dea67d25b9ecb883c8edd
Aula <Aula@EQUIP079574.xunta.local>
2020-10-22 13:42:16 +0200

    Tratamiento de excepciones con try-catch.

--- excepciones.cpp
+++ excepciones.cpp
@@ -1,40 +1,37 @@
-// calc_err.cpp: Tests the function calc(),
-// which throws exceptions.
-// ----------------------------------------------------
 #include <iostream>
 #include <string>
+#include <stdexcept>
+
 using namespace std;
-double calc( int a, int b );
+
+
+double calc( int a, int b ) {
+    if (b == 0) throw logic_error("División entre cero.");
+    return (double)a / b;
+}
+
+
 int main() {
     int x, y;
     double res;
-    bool flag = false;
-    do
-    {
-      try                                 // try block
-      {
-        cout << "Enter two positive integers: ";
+
+    try {
+        cout << "Introduzca dos números enteros: ";
         cin >> x >> y;
-        res = calc( x, y);
+        res = calc(x, y);
         cout << x << "/" << y << " = " << res << endl;
-flag = true;
+    } 
+    catch( string& s) {
+        cerr << s << endl;
+    } 
+    catch( logic_error& error) {
+        cerr << error.what() << endl;
+    }
+    catch(...) {
+        cerr << "Error inesperado. \n";
+        exit(1);
+    }
+
+    return 0; 
 }
-catch( string& s)
-{
-  cerr << s << endl;
-}
-catch( Error& )
-{
-// Then to leave the loop.
-         // 1st catch block
-         // 2nd catch block
-        cerr << "Division by 0! " << endl;
-}
-catch(...) // 3rd catch block {
-        cerr << "Unexpected exception! \n";
-exit(1);
-}
-   }while( !flag);
-// continued ...
-return 0; }
  
